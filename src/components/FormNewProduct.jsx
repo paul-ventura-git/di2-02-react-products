43c7484b4020189f56eb61eb1c7477a12ccff781
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-10 13:51:54 -0500

    First commit

---
+++ src/components/FormNewProduct.jsx
@@ -0,0 +1,5 @@
+export default function FormNewProduct() {
+  return (
+    <div>FormNewProduct</div>
+  )
+}
---
+++ src/router/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../components/Layout';
+import Home from '../pages/Home';
+import { productsLoader } from '../services/loaders';
+import FormNewProduct from '../components/FormNewProduct';
+import { newProductAction } from '../services/actions/newProductAction';
+import ProductDetails from '../components/ProductDetails';
+import { productDetailsLoader } from '../services/loaders/productDetailsLoader';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        loader: productsLoader,
+        children: [
+          { path: '/create-product', element: <FormNewProduct />, action: newProductAction },
+          { path: '/:id', element: <ProductDetails />, loader: productDetailsLoader}
+        ],
+      },
+    ],
+  },
+]);
