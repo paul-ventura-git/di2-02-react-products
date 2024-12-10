3d195fea32721ef422fd0396cd10cf699f23caeb
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-10 18:51:30 -0500

    Modal

--- src/components/FormNewProduct.jsx
+++ src/components/FormNewProduct.jsx
@@ -1,6 +1,29 @@
+import { Link, Form } from 'react-router-dom'
+
+import Modal from './Modal'
+import classes from '../assets/css/Modal.module.css'
+
 export default function FormNewProduct() {
   return (
-    <div>FormNewProduct      
-    </div>
+    <Modal>
+      <Form method="post" className={classes.form}>
+        <p>
+          <label htmlFor="body">Text:</label>
+          <textarea id="body" name="body" required rows={3}/>
+        </p>
+        <p>
+          <label htmlFor="name">Your name:</label>
+          <input type="text" id="name" name="author" required />
+        </p>
+        <div className={classes.actions}>
+          <div className={classes.back}>
+            <Link to='..'>
+              Cancel
+            </Link>
+          </div>
+          <button>Submit</button>
+        </div>
+      </Form>
+    </Modal>
   )
 }
